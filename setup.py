cf425b2744cc387f4cb60760b8b031b4d86513c5
Michał Karzyński <postrational@users.noreply.github.com>
2019-02-19 19:35:19 +0100

    Add long description to package (#11)
    * Add long description to package
    
    * Add MANIFEST.in

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ Setup script for gym-demo.
 
 https://github.com/postrational/gym-demo"
 """
+import os
 
 from setuptools import setup
 
@@ -11,6 +12,10 @@ setup(
     name="gym-demo",
     version="0.1.0",
     description="Explore OpenAI Gym environments.",
+    long_description=open(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
+    ).read(),
+    long_description_content_type="text/markdown",
     author="Michal Karzynski",
     author_email="github@karzyn.com",
     url="https://github.com/postrational/gym-demo",
