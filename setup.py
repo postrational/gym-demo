285df099e9f430f21a405def44d87a73c2bd88f9
Michał Karzyński <postrational@users.noreply.github.com>
2019-06-02 09:35:14 +0200

    Bump version to 0.2.2 (#61)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name="gym-demo",
-    version="0.2.1",
+    version="0.2.2",
     description="Explore OpenAI Gym environments.",
     long_description=open(
         os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
